5cf5d8d17caf4170a60648c0976fe782e3b7e8ab
Richard Baltrusch <richard@baltrusch.net>
2023-07-09 18:39:04 +0200

    Add drop particles to boulder

--- core/src/com/mygdx/game/Boulder.java
+++ core/src/com/mygdx/game/Boulder.java
@@ -1,8 +1,11 @@
 package com.mygdx.game;
 
 import java.util.Objects;
+import java.util.stream.Stream;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
@@ -13,6 +16,7 @@ public class Boulder extends RectEntity implements Actor, Renderable, Disposable
 	private Texture texture;
 	private Sound stoneSound; // boulder cant dispose of the sound itself because it otherwise cant be reset!
 	private Vector2 speed;
+	private float dropTime = 0;
 	private boolean dropping = false;
 
 	protected Boulder(Rectangle rect, Texture texture, Sound stoneSound) {
@@ -27,6 +31,7 @@ public class Boulder extends RectEntity implements Actor, Renderable, Disposable
 			return;
 		}
 
+		dropTime = 0;
 		soundHandler.play(stoneSound);
 		dropping = true;
 	}
@@ -34,13 +39,27 @@ public class Boulder extends RectEntity implements Actor, Renderable, Disposable
 	@Override
 	public void render(Renderer renderer) {
 		renderer.draw(rect, texture);
+
+		// draw dropping particle effect
+		if (dropping) {
+			float height = Math.min(100, 200 * dropTime);
+			float width = 2;
+			Color color = new Color(0.3f, 0.3f, 0.3f, 0.3f);
+			float offset = 10;
+			Stream.iterate(0, x -> x + 1).limit(6)
+					.map(x -> new Rectangle(rect.x + x * offset + 7, rect.y + rect.height + 10, width, height))
+					.forEach(x -> renderer.drawRectangle(x, color));
+//			renderer.draw(, null);
+		}
 	}
 
 	@Override
 	public void update(GameLoop game) {
 		if (dropping) {
 			move(speed);
+			dropTime += Gdx.graphics.getDeltaTime();
 		}
+
 		if (this.overlaps(game.getAiPlayer())) {
 			game.triggerPlayerWin();
 		}
