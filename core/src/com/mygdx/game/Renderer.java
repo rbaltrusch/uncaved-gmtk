f9bcdb229120eea17d17a88c364715553f482a0f
Richard Baltrusch <richard@baltrusch.net>
2023-07-09 16:41:00 +0200

    Remove dead code

--- core/src/com/mygdx/game/Renderer.java
+++ core/src/com/mygdx/game/Renderer.java
@@ -36,7 +36,6 @@ public final class Renderer implements Disposable {
 	}
 
 	public void draw(Rectangle pos, TextureRegion textureRegion) {
-//		draw(pos, textureRegion.getTexture());
 		batch.draw(textureRegion, pos.x, pos.y);
 	}
 
@@ -61,29 +60,13 @@ public final class Renderer implements Disposable {
 		}
 	}
 
-	public void drawCircle(Rectangle pos, float radius, Color color) {
-		drawCircle(pos.x, pos.y, radius, color, true);
-	}
-
-	public void drawCircle(float x, float y, float radius, Color color) {
-		drawCircle(x, y, radius, color, true);
-	}
-
-	public void drawCircle(float x, float y, float radius, Color color, boolean filled) {
-//		shapeRenderer.set(filled ? ShapeType.Filled : ShapeType.Line);
-//		shapeRenderer.setColor(color);
-//		shapeRenderer.circle(x, y, radius);
-	}
-
 	private void startRender() {
 		shapeRenderer.setProjectionMatrix(camera.combined);
 		batch.setProjectionMatrix(camera.combined);
-//		shapeRenderer.begin();
 		batch.begin();
 	}
 
 	private void stopRender() {
-//		shapeRenderer.end();
 		batch.end();
 	}
 
